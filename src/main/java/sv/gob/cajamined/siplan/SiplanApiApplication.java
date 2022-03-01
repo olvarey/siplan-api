b5520b5578d72b837c0a2a4d975536c74658107c
Melvin Reyes <olva.reyes@gmail.com>
2022-03-01 13:43:42 -0600

    first commit

---
+++ src/main/java/sv/gob/cajamined/siplan/SiplanApiApplication.java
@@ -0,0 +1,13 @@
+package sv.gob.cajamined.siplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiplanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiplanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/gob/cajamined/siplan/SiplanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.gob.cajamined.siplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiplanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
