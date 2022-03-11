90fd2411d3be6d9afcb59054a2ab211da46f7851
Melvin Reyes <olva.reyes@gmail.com>
2022-03-11 15:16:51 -0600

    Updated repositories

---
+++ src/main/java/sv/gob/cajamined/siplan/repositories/AnioResultadoRepo.java
@@ -0,0 +1,12 @@
+package sv.gob.cajamined.siplan.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import sv.gob.cajamined.siplan.entities.AnioResultado;
+import sv.gob.cajamined.siplan.entities.AnioResultadoPK;
+
+@Repository
+public interface AnioResultadoRepo extends JpaRepository<AnioResultado, AnioResultadoPK>{
+
+}
---
+++ src/main/java/sv/gob/cajamined/siplan/repositories/UnidadOrganizativaRepo.java
@@ -0,0 +1,11 @@
+package sv.gob.cajamined.siplan.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import sv.gob.cajamined.siplan.entities.UnidadOrganizativa;
+
+@Repository
+public interface UnidadOrganizativaRepo extends JpaRepository<UnidadOrganizativa, Long> {
+
+}
