17d77a68d61b0ac878a3d91e233247741f7c2566
Melvin Reyes <olva.reyes@gmail.com>
2022-05-25 05:40:23 -0600

    Remove security

--- src/main/java/sv/gob/cajamined/siplan/SiplanApiApplication.java
+++ src/main/java/sv/gob/cajamined/siplan/SiplanApiApplication.java
@@ -1,43 +1,13 @@
 package sv.gob.cajamined.siplan;
 
-import java.util.ArrayList;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import sv.gob.cajamined.siplan.entities.Rol;
-import sv.gob.cajamined.siplan.entities.Usuario;
-import sv.gob.cajamined.siplan.repositories.RolRepo;
-import sv.gob.cajamined.siplan.repositories.UsuarioRepo;
 
 @SpringBootApplication
 public class SiplanApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiplanApiApplication.class, args);
-	}
-
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	CommandLineRunner run(UsuarioRepo usuarioRepo, RolRepo rolRepo) {
-		return args -> {
-			Rol rol1 = rolRepo.save(new Rol(null, "ROLE_USER"));
-			Rol rol2 = rolRepo.save(new Rol(null, "ROLE_MANAGER"));
-			Rol rol3 = rolRepo.save(new Rol(null, "ROLE_ADMIN"));
-			Usuario user1 = usuarioRepo.save(new Usuario(null, "Melvin Reyes", "olvarey", "123456", new ArrayList<>()));
-			user1.getRoles().add(rol1);
-			user1.getRoles().add(rol2);
-			user1.getRoles().add(rol3);
-			usuarioRepo.save(user1);
-		};
 	}
 
 }
--- src/main/java/sv/gob/cajamined/siplan/repositories/UsuarioRepo.java
+++
@@ -1,9 +0,0 @@
-package sv.gob.cajamined.siplan.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sv.gob.cajamined.siplan.entities.Usuario;
-
-public interface UsuarioRepo extends JpaRepository<Usuario, Long> {
-	Usuario findByUsuario(String usuario);
-}
