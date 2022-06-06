a86e2cb98154c1c1159d2b34ff5a69329c6ae03f
Melvin Reyes <olva.reyes@gmail.com>
2022-06-06 17:40:19 -0600

    Remove not used dependecies references

--- src/main/java/sv/gob/cajamined/siplan/entities/TipoObjetivo.java
+++ src/main/java/sv/gob/cajamined/siplan/entities/TipoObjetivo.java
@@ -1,17 +1,23 @@
 package sv.gob.cajamined.siplan.entities;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.List;
 
 @Entity
 @Table(name = "tipo_objetivo", schema = "siplan")
