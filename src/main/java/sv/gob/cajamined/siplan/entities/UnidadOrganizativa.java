609cb2c3bd77a1152c3740b45021341fb5937adc
Melvin Reyes <olva.reyes@gmail.com>
2022-06-13 15:32:23 -0600

    Update UnidadOrganizativa entity

--- src/main/java/sv/gob/cajamined/siplan/entities/UnidadOrganizativa.java
+++ src/main/java/sv/gob/cajamined/siplan/entities/UnidadOrganizativa.java
@@ -1,13 +1,21 @@
 package sv.gob.cajamined.siplan.entities;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.io.Serializable;
 
 @Entity
 @Table(name = "unidad_organizativa", schema = "siplan")
